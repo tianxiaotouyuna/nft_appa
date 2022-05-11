3253e6442f062a0afb7253e90f68740cbe1f1115
tianxiao <>
2022-05-11 21:54:15 +0800

    add walletconnect codes                    successful

--- root/pages/Discover.tsx
+++ root/pages/Discover.tsx
@@ -3,11 +3,12 @@ import { View, Text, Platform } from "react-native";
 
 import { useWalletConnect, withWalletConnect } from '@walletconnect/react-native-dapp';
 export const Discover = (props: any) => {
+  const connector = useWalletConnect(); // valid
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: "#f2f2f2",
+        backgroundColor: "red",
         paddingHorizontal: 20,
         justifyContent: "space-between",
         paddingTop:100
