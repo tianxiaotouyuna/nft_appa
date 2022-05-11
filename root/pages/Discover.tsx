c583aded7dfe556a762d5c65d20a38de6709da6f
tianxiao <>
2022-05-11 23:07:52 +0800

    walletcoonect代码successful

--- root/pages/Discover.tsx
+++ root/pages/Discover.tsx
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
-import { View, Text, Platform } from "react-native";
+import { View, Button, Platform } from "react-native";
 
 import { useWalletConnect, withWalletConnect } from '@walletconnect/react-native-dapp';
 export const Discover = (props: any) => {
   const connector = useWalletConnect(); // valid
+  const renderView=()=>{
+    if (!connector.connected) {
+      /**
+       *  Connect! 🎉
+       */
+      return <Button title="Connect" onPress={() => connector.connect()} />;
+    }
+    return <Button title="Kill Session" onPress={() => connector.killSession()} />;
+  }
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: "red",
+        backgroundColor: "#f2f2f2",
         paddingHorizontal: 20,
         justifyContent: "space-between",
         paddingTop:100
       }}
     >
+      {renderView()}
     </View>
   );
 };
