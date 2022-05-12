8b38151ba6a1aa96b8c0e14942b30d361258a7ee
nft <331065255@qq.com>
2022-05-12 13:41:10 +0800

    add SafeAreaView scuessful

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import React from "react";
 import { Platform } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
@@ -24,9 +25,11 @@ const App = () => {
         asyncStorage: AsyncStorage,
       }}
     >
-      <NavigationContainer>
-        <AppNavigation />
-      </NavigationContainer>
+      <SafeAreaView style={{ flex: 1 }} edges={["bottom", "left", "right"]}>
+        <NavigationContainer>
+          <AppNavigation />
+        </NavigationContainer>
+      </SafeAreaView>
     </WalletConnectProvider>
   );
 };
